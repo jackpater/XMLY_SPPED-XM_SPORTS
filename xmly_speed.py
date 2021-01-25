94590c795afc651b041e9e601f1499fae8468fcb
jacky <jackpater666@gmail.com>
2021-01-25 19:27:55 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -1021,9 +1021,9 @@ def telegram_bot(title, content):
     print("\n")
     tg_bot_token = TG_BOT_TOKEN
     tg_user_id = TG_USER_ID
-    if "TG_BOT_TOKEN" in os.environ and "TG_USER_ID" in os.environ:
-        tg_bot_token = os.environ["TG_BOT_TOKEN"]
-        tg_user_id = os.environ["TG_USER_ID"]
+    #if "TG_BOT_TOKEN" in os.environ and "TG_USER_ID" in os.environ:
+    tg_bot_token = os.environ["TG_BOT_TOKEN"]
+    tg_user_id = os.environ["TG_USER_ID"]
     if not tg_bot_token or not tg_user_id:
         print("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")
         return
